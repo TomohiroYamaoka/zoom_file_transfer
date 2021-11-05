8fbd77c24cf2cc16ace5ca663d5429c5f25ca18a
山岡智裕 <>
2021-11-05 09:15:24 +0900

    box転送

--- src/box_dicument.py
+++ src/box_dicument.py
@@ -1,11 +1,23 @@
-from boxsdk.object.collaboration import CollaborationRole
+from boxsdk import JWTAuth
+from boxsdk import Client
 
-user = client.user(user_id='11111')
-collaboration = client.folder(folder_id='22222').collaborate(
-    user, CollaborationRole.VIEWER)
+# Configure JWT auth object
+sdk = JWTAuth(
+    client_id="********************",
+    client_secret="*****************",
+    enterprise_id="00000",
+    jwt_key_id="AAAAAA",
+    rsa_private_key_file_sys_path="****.pem",
+    rsa_private_key_passphrase="******"
+)
 
-collaborator = collaboration.accessible_by
-item = collaboration.item
-has_accepted = 'has' if collaboration.status == 'accepted' else 'has not'
-print('{0} {1} accepted the collaboration to folder "{2}"'.format(
-    collaborator.name, has_accepted, item.name))
+# Get auth client
+client = Client(sdk)
+
+# Set upload values
+file_path = '/testfile/photo.jpg'
+file_name = 'photo.jpg'
+folder_id = '0'
+
+box_file = client.folder(folder_id).upload(file_path, file_name)
+print(vars(box_file))
